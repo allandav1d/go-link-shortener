0d2484f6a0ebce8a8188fa69ba891cc14b63711f
allandav1d <allan.j.k15@gmail.com>
2024-11-25 14:53:31 +0000

    implement routes

---
+++ README.md
@@ -0,0 +1,5 @@
+# GO LinkShortener
+
+## Description
+This is a simple link shortener that uses a REST API to shorten URLs. It is built using the Go programming language.
+Project was created for learning purposes on the course Go by [Rocketseat](https://rocketseat.com.br/).
--- api/api.go
+++ api/api.go
@@ -1,22 +1,41 @@
 package api
 
 import (
+	"encoding/json"
+	"log/slog"
+	"math/rand/v2"
 	"net/http"
+	"net/url"
 
-	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func NewHandler() http.Handler {
+func sendJSON(w http.ResponseWriter, resp Response, status int) {
+	w.Header().Set("Content-Type", "application/json")
+	data, err := json.Marshal(resp)
+	if err != nil {
+		slog.Error("failed to marshal json data", "error", err)
+		sendJSON(w, Response{Error: "something went wrong"}, http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(status)
+	if _, err := w.Write(data); err != nil {
+		slog.Error("failed to write response to client", "error", err)
+		return
+	}
+}
+
+func NewHandler(db map[string]string) http.Handler {
 	r := chi.NewMux()
 
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
 
-	r.Post("/api/shorten", handlePost)
-	r.Get("/{code}", handleGet)
+	r.Post("/api/shorten", handlePost(db))
+	r.Get("/{code}", handleGet(db))
 
 	return r
 }
@@ -30,10 +49,48 @@ type Response struct {
 	Data  any    `json:"data,omitempty"`
 }
 
-func handlePost(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello, world"))
+func handlePost(db map[string]string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var body PostBody
+		if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+			sendJSON(w, Response{Error: "invalid request body"}, http.StatusUnprocessableEntity)
+			return
+		}
+
+		if _, err := url.Parse(body.URL); err != nil {
+			sendJSON(w, Response{Error: "invalid URL"}, http.StatusBadRequest)
+			return
+		}
+
+		code := genCode()
+		db[code] = body.URL
+
+		sendJSON(w, Response{Data: code}, http.StatusCreated)
+	}
 }
 
-func handleGet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello, world"))
+const caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func genCode() string {
+	const n = 8
+	byts := make([]byte, 8)
+
+	for i := range n {
+		byts[i] = caracteres[rand.IntN(len(caracteres))]
+	}
+
+	return string(byts)
+}
+
+func handleGet(db map[string]string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		code := chi.URLParam(r, "code")
+		url, ok := db[code]
+		if !ok {
+			sendJSON(w, Response{Error: "not found"}, http.StatusNotFound)
+			return
+		}
+
+		http.Redirect(w, r, url, http.StatusPermanentRedirect)
+	}
 }
