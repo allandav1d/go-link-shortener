602cb36910e2a9addf8472db310173260995f0b0
allandav1d <allan.j.k15@gmail.com>
2024-11-25 13:27:50 +0000

    init

---
+++ m.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+	"time"
+
+	"github.com/allandav1d/go-link-shortener/api"
+)
+
+func main() {
+	if err := run(); err != nil {
+		slog.Error("failed to execute code", "error", err)
+		return
+	}
+	slog.Info("all systems offline")
+}
+
+func run() error {
+	handler := api.NewHandler()
+
+	s := http.Server{
+		ReadTimeout:  10 * time.Second,
+		IdleTimeout:  1 * time.Minute,
+		WriteTimeout: 10 * time.Second,
+		Addr:         ":8080",
+		Handler:      handler,
+	}
+
+	if err := s.ListenAndServe(); err != nil {
+		return err
+	}
+
+	return nil
+}
